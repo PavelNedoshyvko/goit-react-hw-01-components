5eebed69b4bd1815aa8bb7452222584711e32418
Pavel Nedoshyvko <plight83@gmail.com>
2023-10-12 11:32:21 +0300

    update profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,7 +6,7 @@ import {
 	Statistic,
 	StatisticItem,
 	Span
-} from "./Profile.styled"
+} from './Profile.styled'
 
 export const Profile = ({ user: { username, tag, location, avatar, stats: { followers, views, likes } } }) => {
 	return (
